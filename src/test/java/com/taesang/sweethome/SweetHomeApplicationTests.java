021212b5cb8ffe69362e255d260635d23a1e4dd3
Taesang <johnbuytg@naver.com>
2024-05-17 19:18:53 +0900

    Initial commit

---
+++ src/test/java/com/taesang/sweethome/SweetHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taesang.sweethome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
